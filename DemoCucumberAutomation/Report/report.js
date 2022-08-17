8470617c3ff6e728ed69d7565e99b8e36135b59a
saravanangituser <88486159+saravanangituser@users.noreply.github.com>
2022-08-17 13:19:42 +0530

    Add files via upload
    First commit

---
+++ DemoCucumberAutomation/Report/report.js
@@ -0,0 +1,155 @@
+$(document).ready(function() {var formatter = new CucumberHTML.DOMFormatter($('.cucumber-report'));formatter.uri("C:/Users/Saravanakumar_Thanga/PIP_Program/DemoCucumberAutomation/src/test/java/com/demo/features/DemoForm.feature");
+formatter.feature({
+  "line": 1,
+  "name": "Check the Demo QA for user functionality",
+  "description": "",
+  "id": "check-the-demo-qa-for-user-functionality",
+  "keyword": "Feature"
+});
+formatter.before({
+  "duration": 2665111500,
+  "status": "passed"
+});
+formatter.background({
+  "line": 3,
+  "name": "Common Url launch",
+  "description": "",
+  "type": "background",
+  "keyword": "Background"
+});
+formatter.step({
+  "line": 4,
+  "name": "User enter Demo url",
+  "keyword": "Given "
+});
+formatter.match({
+  "location": "DemoQAStep.user_enter_Demo_url()"
+});
+formatter.result({
+  "duration": 10509051300,
+  "status": "passed"
+});
+formatter.scenario({
+  "line": 6,
+  "name": "login into Demo QA  website and Enter basic details",
+  "description": "",
+  "id": "check-the-demo-qa-for-user-functionality;login-into-demo-qa--website-and-enter-basic-details",
+  "type": "scenario",
+  "keyword": "Scenario"
+});
+formatter.step({
+  "line": 8,
+  "name": "User enter \"Saravana\" as Fullname and \"mail1234@gmail.com\" as Email",
+  "keyword": "When "
+});
+formatter.step({
+  "line": 9,
+  "name": "user enter \"Chennai \" as current address and \" Dindigul \" ad Permanent address",
+  "keyword": "And "
+});
+formatter.step({
+  "line": 10,
+  "name": "user able to click the submit button",
+  "keyword": "And "
+});
+formatter.step({
+  "line": 11,
+  "name": "User able to verify the details",
+  "keyword": "Then "
+});
+formatter.match({});
+formatter.result({
+  "status": "undefined"
+});
+formatter.match({});
+formatter.result({
+  "status": "undefined"
+});
+formatter.match({
+  "location": "DemoQAStep.user_able_to_click_the_submit_button()"
+});
+formatter.result({
+  "status": "skipped"
+});
+formatter.match({
+  "location": "DemoQAStep.user_able_to_verify_the_details()"
+});
+formatter.result({
+  "status": "skipped"
+});
+formatter.after({
+  "duration": 1038568100,
+  "status": "passed"
+});
+formatter.before({
+  "duration": 1824128600,
+  "status": "passed"
+});
+formatter.background({
+  "line": 3,
+  "name": "Common Url launch",
+  "description": "",
+  "type": "background",
+  "keyword": "Background"
+});
+formatter.step({
+  "line": 4,
+  "name": "User enter Demo url",
+  "keyword": "Given "
+});
+formatter.match({
+  "location": "DemoQAStep.user_enter_Demo_url()"
+});
+formatter.result({
+  "duration": 17738795700,
+  "status": "passed"
+});
+formatter.scenario({
+  "line": 13,
+  "name": "login into Demo QA  website and verify the option",
+  "description": "",
+  "id": "check-the-demo-qa-for-user-functionality;login-into-demo-qa--website-and-verify-the-option",
+  "type": "scenario",
+  "keyword": "Scenario"
+});
+formatter.step({
+  "line": 16,
+  "name": "User able to click Check Box option",
+  "keyword": "When "
+});
+formatter.step({
+  "line": 17,
+  "name": "User to click Expand button",
+  "keyword": "And "
+});
+formatter.step({
+  "line": 18,
+  "name": "user able to verify the option",
+  "keyword": "Then "
+});
+formatter.match({
+  "location": "DemoQAStep.user_able_to_click_Check_Box_option()"
+});
+formatter.result({
+  "duration": 141503300,
+  "status": "passed"
+});
+formatter.match({
+  "location": "DemoQAStep.user_to_click_Expand_button()"
+});
+formatter.result({
+  "duration": 99080400,
+  "status": "passed"
+});
+formatter.match({
+  "location": "DemoQAStep.user_able_to_verify_the_option()"
+});
+formatter.result({
+  "duration": 34069900,
+  "status": "passed"
+});
+formatter.after({
+  "duration": 997297100,
+  "status": "passed"
+});
+});
